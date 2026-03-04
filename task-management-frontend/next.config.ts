950fa6ef9d75e4ca61b16dbfde2ff3c7afa4cbc3
Priyanka Patel <ppriyankapatel58@gmail.com>
2026-03-04 16:05:59 +0530

    gonfig change

--- task-management-frontend/next.config.ts
+++ task-management-frontend/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  trailingSlash: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', 
+      },
+    ],
+  },
+  
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  }
 };
 
 export default nextConfig;
